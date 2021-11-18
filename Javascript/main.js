d17272768379c4f0c896baa3e7794f724143f02d
Natalia Castiblanco <castiblanconattalia@gmail.com>
2021-11-17 20:12:09 -0500

    Formulario contacto

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,22 +1,5 @@
 //Aplicación en la cual un usuario ingresa horas trabajadas, el valor de cada hora y muestra
-            //el total a pagar y los minutos que trabajo.
-            //int Horast;
-            //int ValorH;
-            //int Minutost;
-            //Double Sueldo;
-
-            //Console.WriteLine("Digite el numero de horas trabajadas: ");
-            //Horast = int.Parse(Console.ReadLine());
-
-            //Console.WriteLine("Digite el valor pagado por cada hora: ");
-            //ValorH = int.Parse(Console.ReadLine());
-
-            //Minutost = Horast * 60;
-            //Sueldo = Horast * ValorH;
-
-            //Console.WriteLine("El total a pagar por sus horas trabajadas es: " + Sueldo);
-            //Console.WriteLine("Los minutos que trabajo fueron: " + Minutost);
-
+        
     var horasT = prompt("Digite el numero de horas trabajadas: ");
     var valorH = prompt("Digite el valor pagado por cada hora: ");
 
@@ -26,5 +9,11 @@
     alert("El total a pagar por sus horas trabajadas es: " + sueldo);
     alert("Los minutos trabajados fueron: " + minutosT);
 
+//Utilizando funciones de JavaScript programar una página en la que el usuario ingrese una
+//palabra y pueda consultar:
+//La longitud de la palabra
+//La palabra convertida en mayúsculas
+//La palabra convertida en minúsculas
+//El primer carácter de la palabra
 
     
